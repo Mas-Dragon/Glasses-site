475efb024829b8023db0ef23186ae73739e6328e
Muhammed numan <159151306+Mas-Dragon@users.noreply.github.com>
2026-05-07 01:02:34 +0300

    Fix GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Landing from "./Landing";
 import Products from "./Products";
 import Product from "./Product";
@@ -7,8 +7,7 @@ import Checkout from "./Checkout";
 
 export default function App() {
     return (
-
-        <BrowserRouter basename="/Glasses-site">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/products" element={<Products />} />
@@ -16,6 +15,6 @@ export default function App() {
                 <Route path="/specs" element={<Specs />} />
                 <Route path="/checkout" element={<Checkout />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
